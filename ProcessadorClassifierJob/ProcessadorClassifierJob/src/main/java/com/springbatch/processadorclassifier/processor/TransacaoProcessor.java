6bdf2592cfe9c385bef30d385c545e66ddfdfbda
indiesbr <noisabreus@hotmail.com>
2026-03-15 08:31:34 -0300

    add porcessador composto com classificador

---
+++ ProcessadorClassifierJob/ProcessadorClassifierJob/src/main/java/com/springbatch/processadorclassifier/processor/TransacaoProcessor.java
@@ -0,0 +1,13 @@
+package com.springbatch.processadorclassifier.processor;
+
+import com.springbatch.processadorclassifier.dominio.Transacao;
+import org.springframework.batch.item.ItemProcessor;
+
+public class TransacaoProcessor implements ItemProcessor<Transacao,Transacao> {
+
+    @Override
+    public Transacao process(Transacao transacao) throws Exception {
+        System.out.println(String.format("Aplicando regras de negocio na transacao %s",transacao.getId()));
+        return transacao;
+    }
+}
